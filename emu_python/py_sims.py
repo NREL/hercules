c894e5d7ed249ecf32ae492d08d5dea5bd8eb600
Paul <paul.fleming@nrel.gov>
2023-02-07 13:34:56 -0700

    New file structure

---
+++ emu_python/py_sims.py
@@ -0,0 +1,16 @@
+
+
+
+class PySims():
+
+    def __init__(self):
+        pass
+
+    def step(self, inputs):
+
+        # Calls individual python simulations
+
+        outputs = None
+
+        return outputs
+

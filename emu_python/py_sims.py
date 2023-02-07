5b1305aaf41fff11a1d35704c21d934a8e98871d
Genevieve Starke <Genevieve.Starke@nrel.gov>
2023-02-07 15:23:45 -0700

    First addition for py sims

--- emu_python/py_sims.py
+++ emu_python/py_sims.py
@@ -1,10 +1,20 @@
-
+from emu_python.python_simulators.simple_solar_model import SimpleSolar
 
 
 class PySims():
 
-    def __init__(self):
-        pass
+    def __init__(self, input_dictionary, dt):
+
+        self.input_dict = input_dictionary['py_sims']
+
+        available_modules = ['SimpleSolar']
+        present_modules = {}
+
+        # Find what technologies are in the input file
+        component_names = list(self.input_dict.keys())
+        for i in component_names:
+            present_modules[i] = self.input_dict[i]['type'](self.input_dict[i], dt)
+        
 
     def step(self, inputs):
 

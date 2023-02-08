60e54b1b8d4258e490dea0af5e30cf1efa016e33
Paul <paul.fleming@nrel.gov>
2023-02-08 16:12:01 -0700

    log error stream

--- example_case_folders/example_sim_05/bash_script.sh
+++ example_case_folders/example_sim_05/bash_script.sh
@@ -9,10 +9,10 @@ helics_broker -t zmq  -f 2 --loglevel="debug" &
 
 # Need to set this to your emu_python folder
 # cd /home/pfleming/emu_python/emu_python
-python3 emu_runscript.py emu_input_000.yaml >> logemu & # Start the controller center and pass in input file
+python3 emu_runscript.py emu_input_000.yaml >> logemu 2>&1 & # Start the controller center and pass in input file
 
 
-python3 emu_runscript_dummy_amr.py >> logdummy &
+python3 emu_runscript_dummy_amr.py >> logdummy 2>&1
 # Now go back to scratch folder and launch the job
 
 # cd /scratch/pfleming/c2c/example_sim_02

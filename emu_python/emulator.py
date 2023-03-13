70a6ea4c1f9a3fd1e103e0cdf6c9fc01bda00a1a
Paul Fleming <pfleming@el3.ib0.cm.hpc.nrel.gov>
2023-03-13 11:34:11 -0600

    Change hard-coded yaw control to 240

--- emu_python/emulator.py
+++ emu_python/emulator.py
@@ -230,7 +230,7 @@ class Emulator(FederateAgent):
         # self.set_wind_speed_direction()
 
         #yaw_angles = [270 for t in range(self.num_turbines)]
-        yaw_angles = [270 for t in range(self.num_turbines)]
+        yaw_angles = [240 for t in range(self.num_turbines)]
         # log these in kafka
         #yaw_angles[1] = 260
 

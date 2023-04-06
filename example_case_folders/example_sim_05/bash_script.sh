712b58defe33d6484b56b3d88586a08034fe36ac
paulf81 <paul.fleming@nrel.gov>
2023-04-06 10:23:52 -0600

    Feature/update dummy amr (#13)
    * Remove unused imports
    
    * expect an amr input file from command line
    
    * pass in amr input file name
    
    * rearrange to get amr input file from command line
    
    * ignore echo files
    
    * Clean up to look more like precursor driven amr

--- example_case_folders/example_sim_05/bash_script.sh
+++ example_case_folders/example_sim_05/bash_script.sh
@@ -12,7 +12,7 @@ helics_broker -t zmq  -f 2 --loglevel="debug" &
 python3 emu_runscript.py emu_input_000.yaml >> logemu 2>&1 & # Start the controller center and pass in input file
 
 
-python3 emu_runscript_dummy_amr.py >> logdummy 2>&1
+python3 emu_runscript_dummy_amr.py amr_input.inp >> logdummy 2>&1
 # Now go back to scratch folder and launch the job
 
 # cd /scratch/pfleming/c2c/example_sim_02

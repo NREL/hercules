eb151ca96ba2a117b12de970a1030e44e4331ffc
Paul Fleming <pfleming@el2.ib0.cm.hpc.nrel.gov>
2023-07-12 15:46:07 -0600

    clean up

--- example_case_folders/example_sim_06/batch_script.sh
+++ example_case_folders/example_sim_06/batch_script.sh
@@ -8,21 +8,17 @@
 
 # A lot of modules and conda stuff
 source /nopt/nrel/apps/anaconda/5.3/etc/profile.d/conda.sh
-# module use /nopt/nrel/apps/modules/centos74/modulefiles
 module use /not/nrel/apps/modules/default/modulefiles
 module purge
-# module load conda/5.3
 module load conda
 export PREFIX=~/.conda-envs/hercules
 export PATH=$PREFIX/bin:$PATH
 export FI_PROVIDER_PATH=$PREFIX/lib/libfabric/prov
 export LD_LIBRARY_PATH=$PREFIX/lib/libfabric:$PREFIX/lib/release_mt:$LD_LIBRARY_PATH
 source activate hercules
-# module load intel-mpi/2018.0.3
 module load helics/helics-3.1.0_openmpi
 module load netcdf-c/4.7.3/gcc-mpi
-# module load mkl
-# module load mpt
+
 
 export HELICS_PORT=32000
 
@@ -35,4 +31,4 @@ python3 hercules_runscript.py hercules_input_000.yaml >> loghercules 2>&1  & # S
 
 # Now go back to scratch folder and launch the job
 # cd /scratch/pfleming/c2c/example_sim_02
-mpirun -n 72 /home/msinner/hercules_moa_dev/amr-wind/build/amr_wind amr_input.inp >> logamr 2>&1 
+mpirun -n 72 /home/pfleming/amr-wind/build/amr_wind amr_input.inp >> logamr 2>&1 

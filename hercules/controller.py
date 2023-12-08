03ff765faddb2be23c8bea3e36376f38f13aa932
misha <msinner@nrel.gov>
2023-12-08 11:09:09 -0700

    Adding back a pass-through controller in case WHOC not available.

---
+++ hercules/controller.py
@@ -0,0 +1,14 @@
+
+
+
+class Controller():
+
+    def __init__(self, input_dict):
+        pass
+
+    def step(self, main_dict):
+
+        # TODO: does there need to be a seperate "controller" dict?
+        # Might make understanding the log easier?
+        return main_dict
+

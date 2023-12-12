ddc2a20b28b570a5d160e06a7d8f92c27c2b4471
Zachary <ztully@nrel.gov>
2023-12-12 08:09:14 -0700

    small change in batch script

--- example_case_folders/06_amr_wind_standin_and_battery/batch_script.sh
+++ example_case_folders/06_amr_wind_standin_and_battery/batch_script.sh
@@ -5,8 +5,6 @@ export HELICS_PORT=32000
 
 helics_broker -f 2 --consoleloglevel=trace --loglevel=debug --local_port=$HELICS_PORT &
 
-
-
 cd example_case_folders/06_amr_wind_standin_and_battery 
 
 python hercules_runscript_dummy_amr.py amr_input.inp amr_standin_data.csv >> logdummy 2>&1 &

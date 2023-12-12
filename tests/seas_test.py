a7c00ea27ba9715ba71166aafbd9ff967e484d0a
paulf81 <paul.fleming@nrel.gov>
2023-12-12 15:04:10 -0700

    Update to public seas (#39)
    * update ci to install seas
    
    * Update install docs
    
    * Add a test of seas and convert to pytest

---
+++ tests/seas_test.py
@@ -0,0 +1,9 @@
+import pytest
+
+from SEAS.federate_agent import FederateAgent
+
+def test_seas_import():
+
+    # Test that can import seas and declare a federate agent
+
+    FE = FederateAgent('test')

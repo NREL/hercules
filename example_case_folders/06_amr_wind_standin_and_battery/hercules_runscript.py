ec60b01677665fcf6808888ee0f2bc21663448d9
Zachary <ztully@nrel.gov>
2023-12-12 16:25:54 -0700

    troubleshooting WHOC format battery controller

--- example_case_folders/06_amr_wind_standin_and_battery/hercules_runscript.py
+++ example_case_folders/06_amr_wind_standin_and_battery/hercules_runscript.py
@@ -6,11 +6,22 @@ import time
 import sys
 
 
-import os
+from whoc.interfaces.hercules_wind_battery_interface import HerculesWindBatteryInterface
+from whoc.controllers.hercules_wind_battery_controller import (
+    HerculesWindBatteryController,
+)
+
+
+# if len(sys.argv) > 1:
+input_dict = load_yaml(sys.argv[1])
+
+
+interface = HerculesWindBatteryInterface(input_dict)
+controller = HerculesWindBatteryController(interface, input_dict)
+
 
 input_dict = load_yaml(sys.argv[1])
 
-controller = Controller(input_dict)
 py_sims = PySims(input_dict)
 
 
--- example_case_folders/06_amr_wind_standin_and_battery/hercules_runscript_dummy_amr.py
+++ example_case_folders/06_amr_wind_standin_and_battery/hercules_runscript_dummy_amr.py
@@ -1,12 +1,26 @@
 import sys
 from hercules.dummy_amr_wind import launch_dummy_amr_wind
 
-# Check that two or fewer command line arguments were given
-if len(sys.argv) > 3:
+
+if len(sys.argv) == 1:
+    amr_input_file = (
+        "example_case_folders/06_amr_wind_standin_and_battery/amr_input.inp"
+    )
+    amr_standin_file = (
+        "example_case_folders/06_amr_wind_standin_and_battery/amr_standin_data.csv"
+    )
+
+    launch_dummy_amr_wind(amr_input_file, amr_standin_file)
+
+elif len(sys.argv) == 2:
+    amr_input_file = sys.argv[1]
+    launch_dummy_amr_wind(amr_input_file)
+
+
+elif len(sys.argv) > 3:
     raise Exception("Usage: python hercules_runscript_dummy_amr.py <amr_input_file>")
 
-# # Get the first command line argument
-# This is the name of the file to read
+
 else:
     if len(sys.argv) > 1:
         amr_input_file = sys.argv[1]
@@ -15,5 +29,4 @@ else:
         amr_standin_file = sys.argv[2]
         print(f"Using standin data for AMR-Wind from file: {amr_standin_file}")
 
-launch_dummy_amr_wind(amr_input_file, amr_standin_file)
-
+        launch_dummy_amr_wind(amr_input_file, amr_standin_file)

7c8f2bc8df4311549b60a6f25d9bfcbca3c8bb9a
misi9170 <39596329+misi9170@users.noreply.github.com>
2024-01-30 18:37:43 -0500

    Add alternative standin for AMR-wind that runs FLORIS (#63)
    * Setting up example to work through.
    
    * bugfix: fix old dummy name.
    
    * Constructs fi object.
    
    * appears to run as expected.
    
    * Various updates for smoother running.
    
    * Ruff.
    
    * Remove gch yaml in favor of a default dictionary defined in floris_standin.py
    
    * Undo ruff format, which fights with ruff.
    
    * Tests for FlorisStandin.
    
    * ruff
    
    * Adding correct floris branch to github workflow for testing.
    
    * try again...
    
    * and again.
    
    * ready to test external data.
    
    * Test for external data built out and passing.
    
    * ruff.
    
    * Remove obselete handling.
    
    * match reconfiguration of turbine package.
    
    * add pip install instructions.
    
    * ruff :(
    
    * Standin power specified in kW; emulator uses correct actuator.
    
    * Update test to reflect kW units.
    
    * remove unused JoukowskyDisk information.
    
    * Adding a comment explaining units in Floris power calculation
    
    * Adjusting to meet Ruff standards
    
    * Line shortened.
    
    ---------
    
    Co-authored-by: Genevieve Starke <Genevieve.Starke@nrel.gov>

--- hercules/amr_wind_standin.py
+++ hercules/amr_wind_standin.py
@@ -184,12 +184,16 @@ class AMRWindStandin(FederateAgent):
             logger.info("Calculating simulation time: %.1f" % sim_time_s)
 
             # Compute the turbine power using a simple formula
+            if self.message_from_server is not None:
+                yaw_angles = self.message_from_server[-self.num_turbines :]
+            else:
+                yaw_angles = None
             (
                 amr_wind_speed,
                 amr_wind_direction,
                 turbine_powers,
                 turbine_wind_directions,
-            ) = self.get_step(sim_time_s)
+            ) = self.get_step(sim_time_s, yaw_angles)
 
             # ================================================================
             # Communicate with control center
@@ -232,7 +236,7 @@ class AMRWindStandin(FederateAgent):
 
     # TODO cleanup code to move publish and subscribe here.
 
-    def get_step(self, sim_time_s):
+    def get_step(self, sim_time_s, yaw_angles=None):
         """Retreive or calculate wind speed, direction, and turbine powers
 
         Input:
@@ -327,7 +331,7 @@ def launch_amr_wind_standin(amr_input_file, amr_standin_data_file=None):
         "endpoint_interval": 1,
         "starttime": 0,
         "stoptime": temp["stop_time"],
-        "Agent": "dummy_amr_wind",
+        "Agent": "amr_wind_standin",
     }
 
     if amr_standin_data_file is not None:

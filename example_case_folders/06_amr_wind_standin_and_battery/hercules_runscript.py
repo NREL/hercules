74b806bd7d1208aaba607303a7a30da32fe19808
Zachary <ztully@nrel.gov>
2024-02-05 10:39:04 -0700

    update WHOC WindBatteryController name

--- example_case_folders/06_amr_wind_standin_and_battery/hercules_runscript.py
+++ example_case_folders/06_amr_wind_standin_and_battery/hercules_runscript.py
@@ -3,8 +3,8 @@ import sys
 from hercules.emulator import Emulator
 from hercules.py_sims import PySims
 from hercules.utilities import load_yaml
-from whoc.controllers.hercules_wind_battery_controller import (
-    HerculesWindBatteryController,
+from whoc.controllers.wind_battery_controller import (
+    WindBatteryController,
 )
 from whoc.interfaces.hercules_wind_battery_interface import HerculesWindBatteryInterface
 
@@ -13,7 +13,7 @@ input_dict = load_yaml(sys.argv[1])
 
 
 interface = HerculesWindBatteryInterface(input_dict)
-controller = HerculesWindBatteryController(interface, input_dict)
+controller = WindBatteryController(interface, input_dict)
 
 
 input_dict = load_yaml(sys.argv[1])

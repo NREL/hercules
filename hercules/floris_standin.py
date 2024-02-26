6fb874aafd832c83d3963e29bbb325484024acb8
misi9170 <michael.sinner@nrel.gov>
2024-02-26 11:02:17 -0700

    Bugfix: generator_efficiency usage changed in FLORIS; this tracks that change.

--- hercules/floris_standin.py
+++ hercules/floris_standin.py
@@ -113,6 +113,7 @@ def construct_floris_from_amr_input(amr_wind_input):
             hub_height=hub_height,
             rotor_diameter=rotor_diameter,
             ref_air_density=ref_air_density,
+            generator_efficiency=1.0,
         )
 
         # load a default model

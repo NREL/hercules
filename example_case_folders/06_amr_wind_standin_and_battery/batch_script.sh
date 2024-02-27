d7f199e24bb986bfd22ce346705eec1c96ee6863
paulf81 <paul.fleming@nrel.gov>
2024-02-27 16:30:58 -0700

    Feature/update example 6 (#84)

--- example_case_folders/06_amr_wind_standin_and_battery/batch_script.sh
+++ example_case_folders/06_amr_wind_standin_and_battery/batch_script.sh
@@ -1,11 +1,48 @@
 #!/bin/bash
 
-export HELICS_PORT=32000 
-helics_broker -f 2 --consoleloglevel=trace --loglevel=debug --local_port=$HELICS_PORT &
+# Ensure hercules conda or venv is activated before running this script
+# Run this script via the command: 
+#    bash batch_script.sh
+#    source batch_script.sh
+#    ./batch_script.sh
 
-cd example_case_folders/06_amr_wind_standin_and_battery 
+# Generate the stand-in data uses a script within hercules/tools
+echo "Generating stand-in data"
+cd ../../
 
-python hercules_runscript_amr_standin.py amr_input.inp amr_standin_data.csv >> logstandin 2>&1 &
+# Make a temp folder to hold the outputs
+mkdir -p outputs
+python hercules/tools/generate_amr_standin_data.py
 
-python hercules_runscript.py hercules_input_000.yaml >> loghercules 2>&1
+# Delete any log files in the output folder
+rm -f outputs/*.log
 
+# Go back to the 06 directory
+cd example_case_folders/06_amr_wind_standin_and_battery
+echo "Finished generating stand-in data"
+
+# Set the helics port to use
+#make sure you use the same port number in the amr_input.inp and hercules_input_000.yaml files
+export HELICS_PORT=32000
+
+# Delete the existing outputs folder
+rm -rf outputs
+
+# Create the outputs folder
+mkdir -p outputs
+
+# Set up the helics broker
+helics_broker -t zmq  -f 2 --loglevel="debug" --local_port=$HELICS_PORT --consoleloglevel=trace & 
+# For debugging add --consoleloglevel=trace
+
+# Start the amr_standin
+echo "Starting amr stand-in"
+python hercules_runscript_amr_standin.py amr_input.inp amr_standin_data.csv >> outputs/logstandin.log 2>&1 &
+
+# Start the controller center and pass in input file
+echo "Starting hercules"
+python hercules_runscript.py hercules_input_000.yaml >> outputs/loghercules.log 2>&1
+
+# If everything is successful
+echo "Finished running hercules"
+exit 0

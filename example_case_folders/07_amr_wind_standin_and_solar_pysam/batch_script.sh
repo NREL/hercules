bcee3c357d19b3205f10fab05bfc95efdedd6c0b
paulf81 <paul.fleming@nrel.gov>
2024-03-12 15:13:05 -0600

    Refactor example 7 (#87)
    * Add README
    
    * Update batch script
    
    * Update deploy pages
    
    * Update toc
    
    * fixing solar_pysam UnicodeDecodeError bug
    
    * Update README
    
    * Back update example 6 script
    
    ---------
    
    Co-authored-by: brookeslawski <brooke.stanislawski@gmail.com>

--- example_case_folders/07_amr_wind_standin_and_solar_pysam/batch_script.sh
+++ example_case_folders/07_amr_wind_standin_and_solar_pysam/batch_script.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
 
-# Example bash for running things locally
-# I just run these one at a t time
+# Ensure hercules conda or venv is activated before running this script
 
-# A lot of modules and conda stuff
-conda activate hercules
+# Run this script via the command: 
+#    bash batch_script.sh
+#    ./batch_script.sh
 
 # Set the helics port to use: 
+#make sure you use the same port number in the amr_input.inp and hercules_input_000.yaml files. 
 export HELICS_PORT=32000
 
-#make sure you use the same port number in the amr_input.inp and hercules_input_000.yaml files. 
+# Delete the logs within the outputs folder (it the output folder exists)
+if [ -d "outputs" ]; then
+  rm -f outputs/*.log
+fi
+
+# Create the outputs folder
+mkdir -p outputs
 
 # Set up the helics broker
 helics_broker -t zmq  -f 2 --loglevel="debug" --local_port=$HELICS_PORT & 
+# For debugging add --consoleloglevel=trace
 
+# Start the amr_standin
+echo "Starting amr stand-in"
+python3 hercules_runscript_amr_standin.py amr_input.inp >> outputs/logstandin.log 2>&1 &
 
-python3 hercules_runscript.py hercules_input_000.yaml >> loghercules 2>&1 & # Start the controller center and pass in input file
+# Start the controller center and pass in input file
+echo "Starting hercules"
+python3 hercules_runscript.py hercules_input_000.yaml >> outputs/loghercules.log 2>&1
 
+# If everything is successful
+echo "Finished running hercules"
+exit 0
 
-python3 hercules_runscript_amr_standin.py amr_input.inp >> logstandin 2>&1
 
 

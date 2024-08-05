2b2c93fbf9043b258ba62f7e89b8fef0d32378b7
brookeslawski <brooke.stanislawski@gmail.com>
2024-08-05 17:34:45 -0600

    minor cleanup

--- example_case_folders/07_floris_standin_and_solar_pysam/batch_script.sh
+++ example_case_folders/07_floris_standin_and_solar_pysam/batch_script.sh
@@ -25,6 +25,7 @@ helics_broker -t zmq  -f 2 --loglevel="debug" --local_port=$HELICS_PORT &
 # Start the controller center and pass in input file
 echo "Starting hercules"
 # python3 hercules_runscript.py hercules_input_000.yaml >> outputs/loghercules.log 2>&1 &
+# python3 hercules_runscript.py hercules_controller_input_000.yaml >> outputs/loghercules.log 2>&1 &
 # python3 hercules_runscript.py hercules_input_OK_ARM.yaml >> outputs/loghercules.log 2>&1 &
 python3 hercules_runscript.py hercules_input_Flatirons.yaml >> outputs/loghercules.log 2>&1 &
 

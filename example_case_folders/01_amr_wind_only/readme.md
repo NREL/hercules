bc3580d29dc1911f03d2c7fa7af193d77a30f606
Paul <paul.fleming@nrel.gov>
2024-11-20 15:33:08 -0700

    Clean up run scripts

--- example_case_folders/01_amr_wind_only/readme.md
+++ example_case_folders/01_amr_wind_only/readme.md
@@ -1,2 +1,3 @@
-This first example runs AMR Wind, using Hercules, but without any additional generation simulated
+This first example runs AMR Wind, using Hercules, but without any additional generation simulated.
 
+Most examples will have the pattern of having an sbatch_script.sh file which calls a run_script.sh file.  The paradigm is that the run_script.sh can be run on a local machine, while the sbatch_script.sh is used to submit the job to a cluster.  However, in this case since AMR-Wind can only be run on a cluster, the sbatch_script.sh is the only script provided.
--- example_case_folders/01_amr_wind_only/sbatch_script.sh
+++ example_case_folders/01_amr_wind_only/sbatch_script.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=36
 #SBATCH --account=ssc
-# #SBATCH --qos=high
+# #SBATCH --qos=high  # Uncomment for higher prioirty
 
 # A lot of modules and conda stuff
 source /nopt/nrel/apps/anaconda/5.3/etc/profile.d/conda.sh
@@ -24,10 +24,7 @@ export HELICS_PORT=32000
 # Set up the helics broker
 helics_broker -t zmq  -f 2 --loglevel="debug" --local_port=$HELICS_PORT & 
 
-# Need to set this to your hercules folder
-# cd /home/pfleming/hercules/hercules
 python3 hercules_runscript.py hercules_input_000.yaml >> loghercules 2>&1  & # Start the controller center and pass in input file
 
-# Now go back to scratch folder and launch the job
-# cd /scratch/pfleming/c2c/example_sim_02
 mpirun -n 72 /home/pfleming/amr-wind/build/amr_wind amr_input.inp >> logamr 2>&1 
+

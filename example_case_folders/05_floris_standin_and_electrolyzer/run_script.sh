d93f9bb1f330236e69302e73b7abf2d1e094d8d4
Starke <genevieve.starke@nrel.gov>
2024-11-21 14:49:36 -0700

    Updating electrolyzer example to work with electrolyzer develop branch

--- example_case_folders/05_floris_standin_and_electrolyzer/run_script.sh
+++ example_case_folders/05_floris_standin_and_electrolyzer/run_script.sh
@@ -38,6 +38,6 @@ mkdir outputs
 export HELICS_PORT=32000
 
 helics_broker -t zmq  -f 2 --loglevel="debug" --local_port=$HELICS_PORT & 
-python3 hercules_runscript_CLcontrol.py hercules_input_000.yaml >> outputs/loghercules_cl 2>&1 &
-python3 floris_runscript.py amr_input.inp floris_standin_data.csv >> outputs/logfloris_cl 2>&1
+python hercules_runscript_CLcontrol.py hercules_input_000.yaml >> outputs/loghercules_cl 2>&1 &
+python floris_runscript.py amr_input.inp floris_standin_data.csv >> outputs/logfloris_cl 2>&1
 

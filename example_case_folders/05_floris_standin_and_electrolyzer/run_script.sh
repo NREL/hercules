0fd0d95447923a37d5f3344ad11a559fea0d8d6e
Paul <paul.fleming@nrel.gov>
2024-11-22 11:42:22 -0700

    Update run_script

--- example_case_folders/05_floris_standin_and_electrolyzer/run_script.sh
+++ example_case_folders/05_floris_standin_and_electrolyzer/run_script.sh
@@ -23,16 +23,11 @@ fi
 
 # Source the Conda initialization script
 source "$CONDA_PATH"
-
-
 conda activate hercules
 
 # Clean up existing outputs
-if [ -f hercules_output_control.csv ]; then rm hercules_output_control.csv; fi
 if [ -d outputs ]; then rm -r outputs; fi
-
-# Create the outputs folder
-mkdir outputs
+mkdir -p outputs
 
 # Generate floris standin data
 python ../../hercules/tools/generate_amr_standin_data.py floris_standin_data.csv
@@ -43,4 +38,19 @@ export HELICS_PORT=32000
 helics_broker -t zmq  -f 2 --loglevel="debug" --local_port=$HELICS_PORT & 
 python hercules_runscript_CLcontrol.py hercules_input_000.yaml >> outputs/loghercules_cl 2>&1 &
 python floris_runscript.py amr_input.inp floris_standin_data.csv >> outputs/logfloris_cl 2>&1
+
+# Clean up helics output if there
+# Search for a file that begins with the current year
+# And ends with csv
+# If the file exists, move to outputs folder
+current_year=$(date +"%Y")
+for file in ${current_year}*.csv; do
+    if [ -f "$file" ]; then
+        mv "$file" outputs/
+    fi
+done
+
+# If everything is successful
+echo "Finished running hercules"
+exit 0
 

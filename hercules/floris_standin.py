937562b993e28df872f64e7a65347e4637cdaa7b
Paul <paul.fleming@nrel.gov>
2024-12-05 14:29:20 -0700

    Remove all copyright statements

--- hercules/floris_standin.py
+++ hercules/floris_standin.py
@@ -1,15 +1,3 @@
-# Copyright 2022 NREL
-
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not
-# use this file except in compliance with the License. You may obtain a copy of
-# the License at http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations under
-# the License.
-
 # This script implements a test client to test out the server against
 # It is based on code from
 # https://github.com/TUDelft-DataDrivenControl/SOWFA/blob/master/exampleCases/example.12.piso.NREL5MW.ADM.zmqSSC.python/ssc/testclient.py

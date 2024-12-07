f22e15152fe168d880e5c0c3a2c09899b2072814
Paul <paul.fleming@nrel.gov>
2024-12-06 21:01:07 -0700

    allow pass in helics_port

--- example_case_folders/08_floris_standin_only/hercules_runscript.py
+++ example_case_folders/08_floris_standin_only/hercules_runscript.py
@@ -5,11 +5,16 @@ from hercules.emulator import Emulator
 from hercules.py_sims import PySims
 from hercules.utilities import load_yaml
 
+# Check that command line arguments are provided
+if len(sys.argv) != 3:
+    raise Exception("Usage: python hercules_runscript.py <hercules_input_file> <helics_port>")
+
 input_dict = load_yaml(sys.argv[1])
 
 # Set the helics port
 helics_port = int(sys.argv[2])
-
+input_dict["hercules_comms"]["helics"]["config"]["helics"]["helicsport"] = helics_port
+print(f"Running Hercules with helics_port {helics_port}")
 
 
 controller = ControllerStandin(input_dict)

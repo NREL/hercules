a96ee566d70197366768fc3982d7eb79a0f00103
Paul <paul.fleming@nrel.gov>
2024-12-11 11:10:51 -0700

    Update example 6

--- example_case_folders/06_amr_wind_standin_and_battery/hercules_runscript_amr_standin.py
+++ example_case_folders/06_amr_wind_standin_and_battery/hercules_runscript_amr_standin.py
@@ -7,4 +7,8 @@ print(f"Running AMR-Wind standin with input file: {amr_input_file}")
 amr_standin_file = sys.argv[2]
 print(f"Using standin data for AMR-Wind from file: {amr_standin_file}")
 
-launch_amr_wind_standin(amr_input_file, amr_standin_file)
+helics_port = int(sys.argv[3])
+print(f"Using HELICS port: {helics_port}")
+
+
+launch_amr_wind_standin(amr_input_file, amr_standin_file, helics_port=helics_port)

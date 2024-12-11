f744d7e8ab7720808a3f3a2f4b36d62d4fbd43a0
Paul <paul.fleming@nrel.gov>
2024-12-11 11:15:04 -0700

    Update example 5

--- example_case_folders/05_floris_standin_and_electrolyzer/run_script.sh
+++ example_case_folders/05_floris_standin_and_electrolyzer/run_script.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
 
-# Kill any active helics jobs
-source ../find_and_kill_helics
+# Locate the scripts folder
+SCRIPTS_DIR="../../scripts"
 
-# Determine the base path for Conda initialization
-if [ -f "/home/$USER/anaconda3/etc/profile.d/conda.sh" ]; then
-    # Common path for Anaconda on Linux
-    CONDA_PATH="/home/$USER/anaconda3/etc/profile.d/conda.sh"
-elif [ -f "/Users/$USER/anaconda3/etc/profile.d/conda.sh" ]; then
-    # Common path for Anaconda on macOS
-    CONDA_PATH="/Users/$USER/anaconda3/etc/profile.d/conda.sh"
-elif [ -f "/opt/anaconda3/etc/profile.d/conda.sh" ]; then
-    # Alternative system-wide installation path
-    CONDA_PATH="/opt/anaconda3/etc/profile.d/conda.sh"
-elif [ -f "opt/miniconda3/etc/profile.d/conda.sh" ]; then
-    # Alternative system-wide installation path
-    CONDA_PATH="/opt/miniconda3/etc/profile.d/conda.sh"
-elif command -v conda &> /dev/null; then
-    # If conda is in PATH, use the which command to find conda location
-    CONDA_PATH=$(dirname "$(which conda)")/../etc/profile.d/conda.sh
-else
-    echo "Conda installation not found. Please ensure Conda is installed and in your PATH."
-    exit 1
-fi
+# Kill any active helics jobs by calling the find_and_kill_helics script
+# within the scripts folder
+source $SCRIPTS_DIR/find_and_kill_helics.sh
 
-# Source the Conda initialization script
-source "$CONDA_PATH"
-conda activate hercules
+# Run the activate CONDA script within the scripts folder
+# to ensure the Hercules environment is active
+source $SCRIPTS_DIR/activate_conda.sh
+
+# Identify an available port for the HELICS broker.  This should
+# be the first in a sequence of 10 available ports
+# In case of comms trouble can be useful to change the first port
+# to check for availability
+first_port=32000
+source $SCRIPTS_DIR/get_helics_port.sh $first_port
 
 # Clean up existing outputs
 if [ -d outputs ]; then rm -r outputs; fi
@@ -35,12 +25,15 @@ mkdir -p outputs
 # Generate floris standin data
 python ../../hercules/tools/generate_amr_standin_data.py floris_standin_data.csv
 
-# Set the helics port to use: 
-export HELICS_PORT=32000
 
+# Set up the helics broker
+echo "Connecting helics broker to port $HELICS_PORT"
 helics_broker -t zmq  -f 2 --loglevel="debug" --local_port=$HELICS_PORT & 
-python hercules_runscript_CLcontrol.py hercules_input_000.yaml >> outputs/loghercules_cl.log 2>&1 &
-python floris_runscript.py amr_input.inp floris_standin_data.csv >> outputs/logfloris_cl.log 2>&1
+#helics_broker -f 2 --consoleloglevel=trace --loglevel=debug --local_port=$HELICS_PORT >> loghelics &
+
+
+python hercules_runscript_CLcontrol.py hercules_input_000.yaml $HELICS_PORT >> outputs/loghercules_cl.log 2>&1 &
+python floris_runscript.py amr_input.inp floris_standin_data.csv $HELICS_PORT >> outputs/logfloris_cl.log 2>&1
 
 # Clean up helics output if there
 # Search for a file that begins with the current year

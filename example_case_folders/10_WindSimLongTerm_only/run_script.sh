e78a99044853e8ff62d1709d56ce70ff44e4d889
Paul <paul.fleming@nrel.gov>
2024-12-16 14:46:12 -0700

    Update runscript

--- example_case_folders/10_WindSimLongTerm_only/run_script.sh
+++ example_case_folders/10_WindSimLongTerm_only/run_script.sh
@@ -3,33 +3,20 @@
 # Locate the scripts folder
 SCRIPTS_DIR="../../scripts"
 
-# Kill any active helics jobs by calling the find_and_kill_helics script
-# within the scripts folder
-source $SCRIPTS_DIR/find_and_kill_helics.sh
+
 
 # Run the activate CONDA script within the scripts folder
 # to ensure the Hercules environment is active
 source $SCRIPTS_DIR/activate_conda.sh
 
-# Identify an available port for the HELICS broker.  This should
-# be the first in a sequence of 10 available ports
-# In case of comms trouble can be useful to change the first port
-# to check for availability
-first_port=32000
-source $SCRIPTS_DIR/get_helics_port.sh $first_port
 
 # Clean up existing outputs
 if [ -d outputs ]; then rm -r outputs; fi
 mkdir -p outputs
 
-# Set up the helics broker
-echo "Connecting helics broker to port $HELICS_PORT"
-helics_broker -t zmq  -f 2 --loglevel="debug" --local_port=$HELICS_PORT & 
-#helics_broker -f 2 --consoleloglevel=trace --loglevel=debug --local_port=$HELICS_PORT >> loghelics &
 
 # Run Hercules
-python hercules_runscript.py hercules_input_000.yaml $HELICS_PORT >> outputs/loghercules.log 2>&1 & # Start the controller center and pass in input file
-python floris_runscript.py inputs/amr_input.inp $HELICS_PORT >> outputs/logfloris.log 2>&1
+python hercules_runscript.py hercules_input_000.yaml >> outputs/loghercules.log 2>&1 & # Start the controller center and pass in input file
 
 # Clean up helics output if there
 # Search for a file that begins with the current year

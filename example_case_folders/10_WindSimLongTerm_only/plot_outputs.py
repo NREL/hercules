2bfc08f88663076b2af4dee750094166434b4083
Paul <paul.fleming@nrel.gov>
2025-01-29 15:35:19 -0700

    Better plots

--- example_case_folders/10_WindSimLongTerm_only/plot_outputs.py
+++ example_case_folders/10_WindSimLongTerm_only/plot_outputs.py
@@ -6,11 +6,63 @@ import pandas as pd
 # Read the Hercules output file
 df = pd.read_csv("outputs/hercules_output.csv", index_col=False)
 
-fig, ax = plt.subplots()
+# Set number of turbines
+n_turbines = 3
+
+# Define a consistent color map with 3 entries
+colors = ["tab:blue", "tab:orange", "tab:green"]
+
+fig, axarr = plt.subplots(2, 1, sharex=True)
+
+# Plot the wind speeds
+ax = axarr[0]
 for t_idx in range(3):
-    ax.plot(df["time"], df[f"py_sims.wind_farm_0.outputs.power_mw.{t_idx:03}"], label=f"Turbine {t_idx}")
+    ax.plot(
+        df["time"],
+        df[f"py_sims.wind_farm_0.outputs.unwaked_velocity.{t_idx:03}"],
+        label=f"Unwaked {t_idx}",
+        color=colors[t_idx],
+    )
 for t_idx in range(3):
-    ax.plot(df["time"], df[f"py_sims.inputs.derating_kw_{t_idx}"]/1000., label=f"Derating {t_idx}", linestyle="--")
+    ax.plot(
+        df["time"],
+        df[f"py_sims.wind_farm_0.outputs.waked_velocity.{t_idx:03}"],
+        label=f"Waked {t_idx}",
+        linestyle="--",
+        color=colors[t_idx],
+    )
+
+# Plot the FLORIS wind speed
+ax.plot(
+    df["time"],
+    df["py_sims.wind_farm_0.outputs.floris_wind_speed"],
+    label="FLORIS",
+    color="black",
+    lw=2,
+)
+
+ax.grid(True)
+ax.legend()
+ax.set_ylabel("Wind Speed [m/s]")
+
+
+# Plot the power
+ax = axarr[1]
+for t_idx in range(3):
+    ax.plot(
+        df["time"],
+        df[f"py_sims.wind_farm_0.outputs.power.{t_idx:03}"],
+        label=f"Turbine {t_idx}",
+        color=colors[t_idx],
+    )
+for t_idx in range(3):
+    ax.plot(
+        df["time"],
+        df[f"py_sims.inputs.derating_{t_idx:03d}"],
+        label=f"Derating {t_idx}",
+        linestyle="--",
+        color=colors[t_idx],
+    )
 ax.grid(True)
 ax.legend()
 ax.set_xlabel("Time [s]")

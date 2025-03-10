a1255f306f73d99124bbe09fe9b833b733be6aa8
Paul <paul.fleming@nrel.gov>
2025-03-10 13:11:15 -0600

    Update plot

--- example_case_folders/12_WindSimLongTerm_RealisticInflow/plot_outputs.py
+++ example_case_folders/12_WindSimLongTerm_RealisticInflow/plot_outputs.py
@@ -6,24 +6,29 @@ import pandas as pd
 # Read the Hercules output file
 df = pd.read_csv("outputs/hercules_output.csv", index_col=False)
 
-# Set number of turbines
-n_turbines = 3
+# Limit to the first 4 hours
+df = df.iloc[:3600 * 4]
 
-# Define a consistent color map with 3 entries
-colors = ["tab:blue", "tab:orange", "tab:green"]
+print(df['py_sims.wind_farm_0.outputs.floris_wind_direction'].head())
+
+# Set number of turbines
+turbines_to_plot = [0,8]
+
+# Define a consistent color map with 9
+colors = ["tab:blue", "tab:orange", "tab:green", "tab:red", "tab:purple", "tab:brown", "tab:pink", "tab:gray", "tab:olive"]
 
 fig, axarr = plt.subplots(2, 1, sharex=True)
 
 # Plot the wind speeds
 ax = axarr[0]
-for t_idx in range(3):
+for t_idx in turbines_to_plot:
     ax.plot(
         df["time"],
         df[f"py_sims.wind_farm_0.outputs.unwaked_velocity.{t_idx:03}"],
         label=f"Unwaked {t_idx}",
         color=colors[t_idx],
     )
-for t_idx in range(3):
+for t_idx in turbines_to_plot:
     ax.plot(
         df["time"],
         df[f"py_sims.wind_farm_0.outputs.waked_velocity.{t_idx:03}"],
@@ -48,14 +53,14 @@ ax.set_ylabel("Wind Speed [m/s]")
 
 # Plot the power
 ax = axarr[1]
-for t_idx in range(3):
+for t_idx in turbines_to_plot:
     ax.plot(
         df["time"],
         df[f"py_sims.wind_farm_0.outputs.power.{t_idx:03}"],
         label=f"Turbine {t_idx}",
         color=colors[t_idx],
     )
-for t_idx in range(3):
+for t_idx in turbines_to_plot:
     ax.plot(
         df["time"],
         df[f"py_sims.inputs.derating_{t_idx:03d}"],

616b8df2b5306b51cd070823c5389031edfe4018
Paul <paul.fleming@nrel.gov>
2025-03-12 11:11:46 -0600

    Run input scripts automatically if inputs don't exist

--- example_case_folders/10_WindSimLongTerm_only/run_script.sh
+++ example_case_folders/10_WindSimLongTerm_only/run_script.sh
@@ -3,8 +3,6 @@
 # Locate the scripts folder
 SCRIPTS_DIR="../../scripts"
 
-
-
 # Run the activate CONDA script within the scripts folder
 # to ensure the Hercules environment is active
 source $SCRIPTS_DIR/activate_conda.sh
@@ -14,8 +12,18 @@ source $SCRIPTS_DIR/activate_conda.sh
 if [ -d outputs ]; then rm -r outputs; fi
 mkdir -p outputs
 
+# If inputs/wind_input.csv does not yet exist, generate it by running generate_wind_history.ipynb from the command line
+if [ ! -f inputs/wind_input.csv ]; then
+    echo "Generating wind history since it does not exist yet"
+    echo "(Running jupyter notebook generate_wind_history.ipynb)"
+    echo "(...This may take a few minutes)"
+    jupyter nbconvert --to notebook --execute generate_wind_history.ipynb
+    echo "Finished generating wind history"
+fi
+
 
 # Run Hercules
+echo "Starting Hercules"
 python hercules_runscript.py hercules_input_000.yaml >> outputs/log_bash.log 2>&1 # Start the controller center and pass in input file
 
 # Clean up helics output if there

38376840646518d44fee195ce2ea25d7925622bb
brookeslawski <brooke.stanislawski@gmail.com>
2025-03-17 11:37:21 -0600

    working long run example with solar, but too slow

--- example_case_folders/13_WindSolarSimLongTerm/plot_outputs.py
+++ example_case_folders/13_WindSolarSimLongTerm/plot_outputs.py
@@ -68,3 +68,18 @@ ax.legend()
 ax.set_xlabel("Time [s]")
 ax.set_ylabel("Power [kW]")
 plt.show()
+
+
+# solar plots
+fig, axs = plt.subplots(2, 1, sharex=True)
+
+axs[0].plot(df["time"], df["py_sims.solar_farm_0.outputs.dni"])
+axs[0].grid(True)
+axs[0].set_ylabel("Irradiance [W/m^2]")
+
+axs[1].plot(df["time"], df["py_sims.solar_farm_0.outputs.power_mw"]*1000, label="solar PV")
+axs[1].grid(True)
+axs[1].legend()
+axs[1].set_xlabel("Time [s]")
+axs[1].set_ylabel("Power [kW]")
+plt.show()

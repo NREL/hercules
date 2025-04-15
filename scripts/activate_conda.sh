602ce09944f4de696f6a4e4ea44e2c924ee39199
brookeslawski <brooke.stanislawski@gmail.com>
2025-04-15 11:29:15 -0600

    undoing user-specific change to activate_conda

--- scripts/activate_conda.sh
+++ scripts/activate_conda.sh
@@ -8,8 +8,7 @@ if [ -f "/home/$USER/anaconda3/etc/profile.d/conda.sh" ]; then
     CONDA_PATH="/home/$USER/anaconda3/etc/profile.d/conda.sh"
 elif [ -f "/Users/$USER/anaconda3/etc/profile.d/conda.sh" ]; then
     # Common path for Anaconda on macOS
-    # CONDA_PATH="/Users/$USER/anaconda3/etc/profile.d/conda.sh"
-    CONDA_PATH="/Users/$USER/miniforge3/etc/profile.d/conda.sh"
+    CONDA_PATH="/Users/$USER/anaconda3/etc/profile.d/conda.sh"
 elif [ -f "/opt/anaconda3/etc/profile.d/conda.sh" ]; then
     # Alternative system-wide installation path
     CONDA_PATH="/opt/anaconda3/etc/profile.d/conda.sh"
@@ -28,4 +27,4 @@ echo "$CONDA_PATH"
 # Source the Conda initialization script
 source "$CONDA_PATH"
 # conda activate hercules
-conda activate hercules-pysam6-whoc
+conda activate hercules
